c467e79b1f94ba6c2686c870636477e0df14ca88
Vivek R <r.vivek@kloctechnologies.co.in>
2019-10-18 17:25:07 +0530

    Created CountryFlag.stories.js

--- src/stories/2-CountryFlag.stories.js
+++ src/stories/2-CountryFlag.stories.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
+import JSXAddon from 'storybook-addon-jsx';
+import { setAddon, storiesOf } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
+
 import CountryFlag from '../CountryFlag';
 
-export default {
-    title: 'CountryFlag',
-}
+// export default {
+//     title: 'CountryFlag',
+// }
 
-export const countryFlag = () => <CountryFlag />
+// export const countryFlag = () => <CountryFlag />
+
+setAddon(JSXAddon);
+
+const stories = storiesOf('CountryFlag', module);
+
+stories.addDecorator(withKnobs);
+
+stories.addWithJSX('Default', () => <CountryFlag />)
