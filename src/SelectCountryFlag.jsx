ea0dfddb5f3c7a58e22efc41182de513340dc97d
rayudus6 <rayudus6@gmail.com>
2019-10-23 12:52:35 +0530

    Select Country: On..

--- src/SelectCountryFlag.jsx
+++ src/SelectCountryFlag.jsx
@@ -23,13 +23,32 @@ export default class SelectCountryFlag extends Component {
             <div>
                 <div>
                     <i>WORLDWIDE</i>
-
+                    <div>
+                        <i>Golbal Site</i>
+                    </div>
                 </div>
                 <div>
                     <i>ASIA</i>
+                    {this.state.data.map((obj, i) => {
+                        return obj.region === 'Asia' ?
+                            (
+                                <div key={obj.name}>
+                                    <img alt="" src={obj.flag} height="20" width="20" />
+                                    <i>{obj.name}</i>
+                                </div>
+                            )
+                            :   null
+                    })}
                 </div>
                 <div>
                     <i>MIDDLE EAST</i>
+                    {this.state.data.map((obj,i) => {
+                        return (
+                            <div key={obj.name}>
+                                {obj.name}
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         )
